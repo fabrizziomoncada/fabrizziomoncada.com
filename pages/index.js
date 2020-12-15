7f7b34099efd438b167c72ff041f24f3bd027c3f
Edgar López <edgar.lr.18@gmail.com>
2020-12-15 11:32:48 -0600

    Add coming soon page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,35 @@
-import React from "react";
+import Head from "next/head";
 
 const Home = () => {
   return (
     <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap"
+          rel="stylesheet"
+        />
+        <title>Fabrizzio Moncada</title>
+      </Head>
       <h1>fabrizziomoncada.com</h1>
+      <p>coming soon...</p>
 
       <style jsx>{`
         div {
+          font-family: "DM Sans", sans-serif;
           display: flex;
           width: 100vw;
           height: 100vh;
           justify-content: center;
           align-items: center;
+          flex-direction: column;
+        }
+        h1 {
+          margin: 0;
+        }
+        p {
+          margin: 0;
+          opacity: 0.5;
         }
       `}</style>
     </div>
