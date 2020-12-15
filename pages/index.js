3a5394e47ed28040b7949bac57a17c58f2be6c5a
Edgar López <edgar.lr.18@gmail.com>
2020-12-15 11:22:18 -0600

    Add react, react-dom and next

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>fabrizziomoncada.com</h1>
+
+      <style jsx>{`
+        div {
+          display: flex;
+          width: 100vw;
+          height: 100vh;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Home;
