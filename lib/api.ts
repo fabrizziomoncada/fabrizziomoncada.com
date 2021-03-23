851b8dbfb68719013f4db4570fb4c73c3894555e
Edgar López <edgar.lr.18@gmail.com>
2021-03-23 17:56:26 -0600

    Add prettier

--- lib/api.ts
+++ lib/api.ts
@@ -1,52 +1,52 @@
-import fs from "fs";
-import { join } from "path";
-import matter from "gray-matter";
+import fs from 'fs'
+import { join } from 'path'
+import matter from 'gray-matter'
 
-const projectsDir = join(process.cwd(), "projects");
+const projectsDir = join(process.cwd(), 'projects')
 
 export function getProjectsSlugs() {
-  return fs.readdirSync(projectsDir);
+  return fs.readdirSync(projectsDir)
 }
 
 export function getProjectBySlug(slug: string, fields: string[] = []) {
-  const realSlug = slug.replace(/\.md$/, "");
-  const fullPath = join(projectsDir, `${realSlug}.md`);
-  const fileContent = fs.readFileSync(fullPath, "utf8");
-  const { data, content } = matter(fileContent);
+  const realSlug = slug.replace(/\.md$/, '')
+  const fullPath = join(projectsDir, `${realSlug}.md`)
+  const fileContent = fs.readFileSync(fullPath, 'utf8')
+  const { data, content } = matter(fileContent)
 
   type Items = {
-    [key: string]: string;
-  };
+    [key: string]: string
+  }
 
-  const items: Items = {};
+  const items: Items = {}
 
   fields.forEach((field) => {
-    if (field === "slug") {
-      items[field] = realSlug;
+    if (field === 'slug') {
+      items[field] = realSlug
     }
-    if (field === "content") {
-      items[field] = content;
+    if (field === 'content') {
+      items[field] = content
     }
     if (data[field]) {
-      items[field] = data[field];
+      items[field] = data[field]
     }
-  });
+  })
 
-  return items;
+  return items
 }
 
 export function getAllProjects(fields: string[] = []) {
-  const slugs = getProjectsSlugs();
+  const slugs = getProjectsSlugs()
   const projects = slugs
     .map((slug) => getProjectBySlug(slug, fields))
-    .sort((project1, project2) => (project1.date < project2.date ? 1 : -1));
-  return projects;
+    .sort((project1, project2) => (project1.date < project2.date ? 1 : -1))
+  return projects
 }
 
 export function getProjects() {
-  const fileDir = join(process.cwd(), "lib/projects.json");
-  const content = fs.readFileSync(fileDir, "utf-8");
-  const response = JSON.parse(content);
+  const fileDir = join(process.cwd(), 'lib/projects.json')
+  const content = fs.readFileSync(fileDir, 'utf-8')
+  const response = JSON.parse(content)
 
-  return response;
+  return response
 }
