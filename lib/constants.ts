5bf18b6b4c4dd2b1d19ed425e04c59c3f1022499
Edgar López <edgar.lr.18@gmail.com>
2021-03-23 19:07:21 -0600

    Add baseUrl and update social links

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,8 +3,8 @@ export const TWITTER_USERNAME = '@edgarlr'
 
 // Social Links
 export const TwitterURL = 'https://twitter.com/edgarlr_'
-export const GithubURL = 'https://github.com/edgarlr'
-export const FigmaURL = 'https://www.figma.com/@edgarlr'
-export const DribbbleURL = 'https://dribbble.com/edgarlr'
-export const LinkedinURL = 'https://www.linkedin.com/in/edgarlr/'
+export const FigmaURL = 'https://www.figma.com/@fabrizzio'
+export const DribbbleURL = 'https://dribbble.com/fabrizziomoncada'
+export const LinkedinURL = 'https://www.linkedin.com/in/fabrizziomoncada'
+export const instagramURL = 'https://www.instagram.com/fabrizzio.design'
 export const EmailURL = 'mailto:edgar18.lr@gmail.com'
