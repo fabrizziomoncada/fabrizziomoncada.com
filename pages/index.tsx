47c52195f870d64f2b4ca952befd89103aa2ced5
Edgar López <edgar.lr.18@gmail.com>
2021-03-23 22:11:53 -0600

    Fix fonts folder error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,11 @@
-import Head from 'next/head'
+import Layout from 'components/Layout/Layout'
 
 const Home = () => {
   return (
-    <div>
+    <Layout>
       <h1>fabrizziomoncada.com</h1>
       <p>coming soon...</p>
-
-      <style jsx>{`
-        div {
-          font-family: 'DM Sans', sans-serif;
-          display: flex;
-          width: 100vw;
-          height: 100vh;
-          justify-content: center;
-          align-items: center;
-          flex-direction: column;
-        }
-        h1 {
-          margin: 0;
-        }
-        p {
-          margin: 0;
-          opacity: 0.5;
-        }
-      `}</style>
-    </div>
+    </Layout>
   )
 }
 
