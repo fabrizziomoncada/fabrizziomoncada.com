84737a51bde252311d8d464adbf9c542c1c304cc
Edgar López <edgar.lr.18@gmail.com>
2021-03-29 23:18:05 -0600

    Add hide header on scroll

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,11 +1,15 @@
 import Globe from 'components/icons/Globe'
+import { useHideOnScroll } from 'lib/hooks/use-hide-on-scroll'
 import { IconButton } from '../ui/IconButton'
 import s from './Header.module.css'
 import ThemeChanger from './ThemeChanger'
+import cn from 'classnames'
 
 const Header = () => {
+  const { isHidden } = useHideOnScroll()
+
   return (
-    <header className={s.root}>
+    <header className={cn(s.root, { [s.hide]: isHidden })}>
       <IconButton href="/" ariaLabel="Go to home">
         <Globe />
       </IconButton>
