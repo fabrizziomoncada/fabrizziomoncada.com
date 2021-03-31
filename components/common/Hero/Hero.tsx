8b07061c6ff5f9f81950165203af5925c8a38f4d
Edgar López <edgar.lr.18@gmail.com>
2021-03-30 21:30:24 -0600

    Add cdmx map to hero

--- components/common/Hero/Hero.tsx
+++ components/common/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+import CDMXMap from 'components/icons/CDMXMap'
 import s from './Hero.module.css'
 
 const Hero = () => {
@@ -8,6 +9,9 @@ const Hero = () => {
         Designer based in <strong>Mexico City.</strong>
       </h1>
       <p className={s.tagline}>silohuette of Mexico City</p>
+      <div className={s.map}>
+        <CDMXMap />
+      </div>
     </section>
   )
 }
