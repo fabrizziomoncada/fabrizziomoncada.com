1cbbf9d5f8e9815eb89604109a4e567bfd6050aa
Edgar López <edgar.lr.18@gmail.com>
2021-04-22 15:46:57 -0500

    Add Linkedin to footer

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import Behance from 'components/icons/Behance'
 import Dribbble from 'components/icons/Dribbble'
 import Figma from 'components/icons/Figma'
 import Instagram from 'components/icons/Instragram'
+import Linkedin from 'components/icons/Linkedin'
 import Mail from 'components/icons/Mail'
 import {
   DribbbleURL,
@@ -9,6 +10,7 @@ import {
   FigmaURL,
   instagramURL,
   BehanceURL,
+  LinkedinURL,
 } from 'lib/constants'
 import { IconButton } from '../ui/IconButton'
 import s from './Footer.module.css'
@@ -18,20 +20,20 @@ const Footer = () => {
     <footer className={s.root}>
       <p className={s.legend}>Be nice :)</p>
       <div className={s.linksContainer}>
-        <IconButton href={instagramURL} external ariaLabel="link to instagram">
-          <Instagram />
-        </IconButton>
         <IconButton href={FigmaURL} external ariaLabel="Link to Figma">
           <Figma />
+        </IconButton>
+        <IconButton href={BehanceURL} external ariaLabel="Link to Behance">
+          <Behance />
         </IconButton>
         <IconButton href={EmailURL} external ariaLabel="Send an email">
           <Mail />
         </IconButton>
-        <IconButton href={BehanceURL} external ariaLabel="Link to Instagram">
-          <Behance />
+        <IconButton href={LinkedinURL} external ariaLabel="Link to Linkedin">
+          <Linkedin />
         </IconButton>
-        <IconButton href={DribbbleURL} external ariaLabel="Link to Dribbble">
-          <Dribbble />
+        <IconButton href={instagramURL} external ariaLabel="link to instagram">
+          <Instagram />
         </IconButton>
       </div>
     </footer>
